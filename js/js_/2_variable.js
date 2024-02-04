3ac699014e7a1372c2c6a2af8296eaba50394c32
yoonho <qufdbsgh431@gmail.com>
2024-02-04 22:46:42 +0900

    spring.io clone init

---
+++ js/js_/2_variable.js
@@ -0,0 +1,19 @@
+var name = '변수';
+
+console.log(name);
+
+var age = 22;
+
+console.log(age);
+
+
+let team = "gaen"; // let, var은 변경 가능, 선언만 하면 undefined
+
+team = "aa"
+console.log(team);
+
+const time = 'gaen'; // const는 변경 불가능, 할당 하지 않으면 선언 불가
+// time = a;
+
+
+
---
+++ js/js_/3_data_types.js
@@ -0,0 +1,8 @@
+const age = 32;
+const temper = -22;
+const poi = 2.22;
+
+console.log(age);
+console.log(temper);
+console.log(poi);
+console.log(typeof(poi));
